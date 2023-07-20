4ca76beca9a534a35853d971e7a98643e27b9e7b
HoaiPhuoGn <92800582+HoaiPhuoGn@users.noreply.github.com>
2023-07-20 21:45:55 +0700

    style ui

---
+++ FrameMates_Studio_UI/Pages/Logout.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrameMates_Studio_UI.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            Response.Cookies.Delete("accessToken");
+            Response.Cookies.Delete("refreshToken");
+
+            return RedirectToPage("Login");
+        }
+    }
+}
